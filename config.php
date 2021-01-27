cb34f724e9ae93b6a0182098bf2d80d6d9466b4e
sergeevdevel <vladdementyev@ya.ru>
2021-01-27 11:52:30 +0300

    правка конфигов

--- config.php
+++ config.php
@@ -1,22 +1,22 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://opencart23/');
+define('HTTP_SERVER', 'http://'.$_SERVER['HTTP_HOST'].'/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://opencart23/');
+define('HTTPS_SERVER', 'https://'.$_SERVER['HTTP_HOST'].'/');
 
 // DIR
-define('DIR_APPLICATION', 'C:/OpenServer/domains/opencart23/catalog/');
-define('DIR_SYSTEM', 'C:/OpenServer/domains/opencart23/system/');
-define('DIR_IMAGE', 'C:/OpenServer/domains/opencart23/image/');
-define('DIR_LANGUAGE', 'C:/OpenServer/domains/opencart23/catalog/language/');
-define('DIR_TEMPLATE', 'C:/OpenServer/domains/opencart23/catalog/view/theme/');
-define('DIR_CONFIG', 'C:/OpenServer/domains/opencart23/system/config/');
-define('DIR_CACHE', 'C:/OpenServer/domains/opencart23/system/storage/cache/');
-define('DIR_DOWNLOAD', 'C:/OpenServer/domains/opencart23/system/storage/download/');
-define('DIR_LOGS', 'C:/OpenServer/domains/opencart23/system/storage/logs/');
-define('DIR_MODIFICATION', 'C:/OpenServer/domains/opencart23/system/storage/modification/');
-define('DIR_UPLOAD', 'C:/OpenServer/domains/opencart23/system/storage/upload/');
+define('DIR_APPLICATION', $_SERVER['DOCUMENT_ROOT'] . '/catalog/');
+define('DIR_SYSTEM', $_SERVER['DOCUMENT_ROOT'] . '/system/');
+define('DIR_IMAGE', $_SERVER['DOCUMENT_ROOT'] . '/image/');
+define('DIR_LANGUAGE', $_SERVER['DOCUMENT_ROOT'] . '/catalog/language/');
+define('DIR_TEMPLATE', $_SERVER['DOCUMENT_ROOT'] . '/catalog/view/theme/');
+define('DIR_CONFIG', $_SERVER['DOCUMENT_ROOT'] . '/system/config/');
+define('DIR_CACHE', $_SERVER['DOCUMENT_ROOT'] . '/system/storage/cache/');
+define('DIR_DOWNLOAD', $_SERVER['DOCUMENT_ROOT'] . '/system/storage/download/');
+define('DIR_LOGS', $_SERVER['DOCUMENT_ROOT'] . '/system/storage/logs/');
+define('DIR_MODIFICATION', $_SERVER['DOCUMENT_ROOT'] . '/system/storage/modification/');
+define('DIR_UPLOAD', $_SERVER['DOCUMENT_ROOT'] . '/system/storage/upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
