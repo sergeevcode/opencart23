51f771b2e05e88ce2b40d5f9f86fcb8df926b41e
sergeevdevel <vladdementyev@ya.ru>
2021-01-28 11:22:47 +0300

    фикс карточки товара

--- assets/js/app.js
+++ assets/js/app.js
@@ -413,39 +413,9 @@ document.addEventListener("DOMContentLoaded", function() {
 	    data: $('#product input[type=\'text\'], #product input[type=\'hidden\'], #product input[type=\'radio\']:checked, #product input[type=\'checkbox\']:checked, #product select, #product textarea'),
 	    dataType: 'json',
 	    
-	    success: function(json) {
-		      $('.alert, .text-danger').remove();
-		      $('.form-group').removeClass('has-error');
-
-		      if (json['error']) {
-		        if (json['error']['option']) {
-		          for (i in json['error']['option']) {
-		            var element = $('#input-option' + i.replace('_', '-'));
-
-		            if (element.parent().hasClass('input-group')) {
-		              element.parent().after('<div class="text-danger">' + json['error']['option'][i] + '</div>');
-		            } else {
-		              element.after('<div class="text-danger">' + json['error']['option'][i] + '</div>');
-		            }
-		          }
-		        }
-
-		        if (json['error']['recurring']) {
-		          $('select[name=\'recurring_id\']').after('<div class="text-danger">' + json['error']['recurring'] + '</div>');
-		        }
-
-		        // Highlight any found errors
-		        $('.text-danger').parent().addClass('has-error');
-		      }
-
+	    success: function(json) { 
 		      if (json['success']) {
-		        $('.breadcrumb').after('<div class="alert alert-success">' + json['success'] + '<button type="button" class="close" data-dismiss="alert">&times;</button></div>');
-
-		        $('#cart > button').html('<span id="cart-total"><i class="fa fa-shopping-cart"></i> ' + json['total'] + '</span>');
-
-		        $('html, body').animate({ scrollTop: 0 }, 'slow');
-
-		        $('#cart > ul').load('index.php?route=common/cart/info ul li');
+		       	$(this).text("В корзине")
 		      }
 		    },
 	        error: function(xhr, ajaxOptions, thrownError) {
